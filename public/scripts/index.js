89820ed5f104758d848b2aba9d518b9bd44ec5eb
akimasa-l <liumingzheng8@outlook.com>
2022-09-18 09:23:29 +0900

    Initial commit

---
+++ public/scripts/index.js
@@ -0,0 +1,61 @@
+function handleTickInitBlue(tick) {
+
+    // set final value, tick will animate towards it
+    setTimeout(function(){
+        tick.value = 125;
+    }, 1000);
+
+    // play with arrive() values to tune animation speed and duration
+    // 1. first value is maximum speed of increase
+    // 2. second value is speed at which the increase ramps up
+
+}
+function handleTickInitYellow(tick) {
+
+    // set final value, tick will animate towards it
+    setTimeout(function(){
+        tick.value = 125;
+    }, 2000);
+
+    // play with arrive() values to tune animation speed and duration
+    // 1. first value is maximum speed of increase
+    // 2. second value is speed at which the increase ramps up
+
+}
+function handleTickInitGreen(tick) {
+
+    // set final value, tick will animate towards it
+    setTimeout(function(){
+        tick.value = 125;
+    }, 3000);
+
+    // play with arrive() values to tune animation speed and duration
+    // 1. first value is maximum speed of increase
+    // 2. second value is speed at which the increase ramps up
+
+}
+
+function handleTickInitWhite(tick) {
+
+    // set final value, tick will animate towards it
+    setTimeout(function(){
+        tick.value = 125;
+    }, 4000);
+
+    // play with arrive() values to tune animation speed and duration
+    // 1. first value is maximum speed of increase
+    // 2. second value is speed at which the increase ramps up
+
+}
+function handleTickInitRed(tick) {
+
+    // set final value, tick will animate towards it
+    setTimeout(function(){
+        tick.value = 125;
+    }, 5000);
+
+    // play with arrive() values to tune animation speed and duration
+    // 1. first value is maximum speed of increase
+    // 2. second value is speed at which the increase ramps up
+
+}
